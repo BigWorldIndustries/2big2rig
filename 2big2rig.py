e1c96d0d3f2ebda5e0d18cfd822aacdc329a9725
Noonz <snunes012@gmail.com>
2024-12-09 13:14:35 -0500

    add bias weighting for categories

--- 2big2rig.py
+++ 2big2rig.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import threading
 import time
 import random
-from bigworld_election_2024 import seed_rates, categories
+from bigworld_election_2024 import seed_rates, categories, categories_by_bias
 
 app = Flask(__name__)
 
@@ -19,17 +19,30 @@ def increment_numbers():
             for key in stored_numbers:
                 total_rate = stored_numbers[key]['rate']
                 # Generate random sub-rates that sum to the total_rate
-                parts = sorted([0, random.random(), random.random(), 1])
-                sub_rates = [parts[i+1] - parts[i] for i in range(3)]
-                
-                # sorting is unnecessary, but we sort so that the smaller share of votes always go to the same categories & the bigger share of votes always go the same categories
-                # this prevents the categories from having an almost equal amount of the total through random distribution over time
-                # this means the categories array should be ordered by desired smallest to biggest categories
-                scaled_sub_rates = sorted([sub_rate * total_rate for sub_rate in sub_rates]) 
-                
-                # Increment each category by its sub-rate
+
+                # Part 1: Population Bias
+                # this is a fixed bias applied so that smaller categories get a smaller random assortment of votes and bigger ones get a bigger random assortment of votes
+                # this prevents votes from being equally distributed in all categories over time due to randomn distribution
+                # categories array should be ordered by desired smallest to biggest categories
+                parts1 = sorted([0] + [random.random() for _ in range(len(categories) - 1)] + [1])
+                sub_rates1 = [parts1[i+1] - parts1[i] for i in range(len(categories))]
+                scaled_sub_rates1 = sorted([sub_rate * total_rate for sub_rate in sub_rates1])
+                # Increment each category by the sub-rate
                 for i, category in enumerate(categories):
-                    stored_numbers[key]['values'][category] += scaled_sub_rates[i]
+                    # divide by 2 because this accounts for half the rate's weighting
+                    stored_numbers[key]['values'][category] += (scaled_sub_rates1[i]) / 2
+
+                # # Part 2: Candidate Bias
+                # # this is a bias applied so certain candidates perform better in certain categories they are deemed as favorites
+                # # this prevents all categories from having the exact same ratio of votes between candidates
+                # # categories_by_bias arrays should be ordered by most favored to least favored factions
+                parts2 = sorted([0] + [random.random() for _ in range(len(categories) - 1)] + [1])
+                sub_rates2 = [parts2[i+1] - parts2[i] for i in range(len(categories))]
+                scaled_sub_rates2 = sorted([sub_rate * total_rate for sub_rate in sub_rates2])
+                # Increment each category by the sub-rate
+                for i, category in enumerate(categories_by_bias[key]):
+                    # divide by 2 because this accounts for half the rate's weighting
+                    stored_numbers[key]['values'][category] += (scaled_sub_rates2[i]) / 2
 
 @app.route('/numbers', methods=['GET'])
 def get_numbers():
--- bigworld_election_2024.py
+++ bigworld_election_2024.py
@@ -6,4 +6,35 @@ seed_rates = {
 }
 
 
-categories = ['maharnegonia', 'zazuland', 'shneibler islands']  # List of categories
+categories = ['WeLoveMatt', 'WeLoveTony', '3rdpartyFTW', 'Shneibler Islands', 'Canada', 'Maharnegonia', 'Zazuland']  # List of categories
+
+# ordered from least favored to most favored
+categories_by_bias = {
+    'Tony': [
+        'WeLoveMatt',
+        'Canada',
+        'Zazuland',
+        'Maharnegonia',
+        'Shneibler Islands',
+        '3rdpartyFTW',
+        'WeLoveTony'
+    ],
+    'BigDaddy': [
+        'WeLoveTony',
+        'Shneibler Islands',
+        'Canada',
+        'Maharnegonia',
+        'Zazuland',
+        '3rdpartyFTW',
+        'WeLoveMatt'
+    ],
+    'TheNightPatrol': [
+        'Maharnegonia',
+        'Shneibler Islands',
+        'WeLoveTony',
+        'WeLoveMatt',
+        'Zazuland',
+        'Canada',
+        '3rdpartyFTW'
+    ]
+}
